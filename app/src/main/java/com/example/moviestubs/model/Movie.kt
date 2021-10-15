ca61baa4866b37b62cd54d21c18e668beab89bc7
Pranav Patel <ppatel@dataminr.com>
2021-10-15 15:29:04 -0400

    feat: creating a movie model and the movie api interface
    Depdencies added: square retrofit2 and gson. The movie model uses gson
    annotations to serialize the movie response keys. The api interface is
    created using retrofit with a gson converter. It is currently exposed as
    a public api object, later we can move this to more of a repository
    pattern so the view model doesn't directly access this api interface.

---
+++ app/src/main/java/com/example/moviestubs/model/Movie.kt
@@ -0,0 +1,59 @@
+package com.example.moviestubs.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    @SerializedName("Id")
+    val id: Int,
+
+    @SerializedName("Name")
+    val name: String,
+
+    @SerializedName("Rank")
+    val rank: Int,
+
+    @SerializedName("Duration")
+    val duration: String,
+
+    @SerializedName("Description")
+    val description: String,
+
+    @SerializedName("Director")
+    val director: String,
+
+    @SerializedName("Genres")
+    val genres: Array<String>,
+
+    @SerializedName("Actors")
+    val actors: Array<String>,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Movie
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (rank != other.rank) return false
+        if (duration != other.duration) return false
+        if (description != other.description) return false
+        if (director != other.director) return false
+        if (!genres.contentEquals(other.genres)) return false
+        if (!actors.contentEquals(other.actors)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + name.hashCode()
+        result = 31 * result + rank
+        result = 31 * result + duration.hashCode()
+        result = 31 * result + description.hashCode()
+        result = 31 * result + director.hashCode()
+        result = 31 * result + genres.contentHashCode()
+        result = 31 * result + actors.contentHashCode()
+        return result
+    }
+}
